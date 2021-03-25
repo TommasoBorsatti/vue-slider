6dddb91c557c70f80f5a96068ae0b3bba7bfaad9
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-25 14:33:48 +0100

    Pronti - Html e Css iniziati, avvio dello script in VUE

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+const App = new Vue (
+
+  {
+    el: "#root",
+
+
+
+    data: {
+
+      pics: [
+        "img/pic6.webp",
+        "img/pic9.jfif",
+        "img/pic11.jpg",
+      ],
+
+      // picIndex: 0;
+
+    },
+
+    method: {
+
+      next: function (){
+
+      }
+    },
+
+
+  }
+);
